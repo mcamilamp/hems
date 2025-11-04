3ac8fb707835883adcfc67f504833b4fd9a4c416
mcamilamp <camimerpa@gmail.com>
2025-11-04 17:28:07 -0500

    Restructuring the administration page

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,34 +1,14 @@
 import "../../styles/admin/admin.scss";
-import { FaHome } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import { FiSettings } from "react-icons/fi";
-import { BiDevices } from "react-icons/bi";
+import SideBarAdmin from "@/components/admin/sideBarAdmin";
+import UsersSection from "@/components/admin/usersSection";
 
 export default function AdminPage() {
-  const menuItems = [
-    { icon: <FaHome className="icon" />, label: "Inicio" },
-    { icon: <FaUser className="icon" />, label: "Usuarios" },
-    { icon: <FiSettings className="icon" />, label: "Configuración" },
-    { icon: <BiDevices className="icon" />, label: "Dispositivos" },
-  ];
-
   return (
     <div className="admin-dashboard">
-      <aside className="sidebar">
-        <h1 className="sidebar-title">Admin dashboard</h1>
-        <nav className="menu">
-          <ul>
-            {menuItems.map((item, index) => (
-              <li key={index} className="menu-item">
-                {item.icon} <span>{item.label}</span>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </aside>
-
+      <SideBarAdmin />
       <main className="main-content">
-        <h1>Main</h1>
+        <h1>Admin Dashboard</h1>
+        <UsersSection />
       </main>
     </div>
   );
---
+++ src/components/admin/sideBarAdmin.jsx
@@ -0,0 +1,29 @@
+import { FaHome } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FiSettings } from "react-icons/fi";
+import { BiDevices } from "react-icons/bi";
+import "../../styles/components/sideBar.scss";
+
+export default function SideBarAdmin() {
+  const menuItems = [
+    { icon: <FaHome className="icon" />, label: "Inicio" },
+    { icon: <FaUser className="icon" />, label: "Usuarios" },
+    { icon: <FiSettings className="icon" />, label: "Configuración" },
+    { icon: <BiDevices className="icon" />, label: "Dispositivos" },
+  ];
+
+  return (
+    <aside className="sidebar">
+      <h1 className="sidebar-title">HEMS</h1>
+      <nav className="menu">
+        <ul>
+          {menuItems.map((item, index) => (
+            <li key={index} className="menu-item">
+              {item.icon} <span>{item.label}</span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
